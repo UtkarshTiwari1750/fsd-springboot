71ff9eaffb28e250cf34f57fa6e91c5d1786bd01
Utkarsh Tiwari <utkarshtiwari1750@gmail.com>
2025-04-17 15:10:14 +0530

    Cron url

--- src/main/java/payment_gateway/demo/CorsConfig.java
+++ src/main/java/payment_gateway/demo/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://fsd-angular.vercel.app/")
+                        .allowedOrigins("https://fsd-angular.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
